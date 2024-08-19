c04dd2247d764d65ce3b70708ec86767cb10fd32
Rocker-Mitchell <rocker.mat94@gmail.com>
2024-08-19 15:07:11 -0600

    Initialize module to manage downloaded resources

---
+++ datasets_util.py
@@ -0,0 +1,41 @@
+"""Module for managing datasets used for model training/testing.
+
+Resources must be downloaded before this module can be used to get data.
+Running this as a script will handle all required downloads.
+"""
+
+# requires galaxy-datasets package
+# https://pypi.org/project/galaxy-datasets/
+
+import os
+from galaxy_datasets import demo_rings
+
+# TODO demo_rings is a small dataset, determine dataset(s) we want
+
+RESOURCES_DIR = "Resources"
+"""The directory to hold all downloaded resources."""
+
+
+def download_resources():
+    """Download required resources so galaxy datasets can be used."""
+    # the whole dataset downloads regardless of train flag
+    demo_rings(os.path.join(RESOURCES_DIR, "demo_rings"), False, True)
+
+
+def get_demo_rings(train: bool = False):
+    """Get demo rings dataset.
+
+    Args:
+        train: A flag to control if the training or testing catalog should be
+            returned. Defaults to False for testing data.
+
+    Returns:
+        A tuple of a catalog DataFrame holding the dataset (with "file_loc"
+        column of absolute image paths) and a list of column names to use as
+        labels.
+    """
+    return demo_rings(os.path.join(RESOURCES_DIR, "demo_rings"), train, False)
+
+
+if __name__ == "__main__":
+    download_resources()
